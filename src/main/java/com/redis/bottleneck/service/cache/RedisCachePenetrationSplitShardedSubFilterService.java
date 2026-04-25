56bd7fed958810b7d76377b4fb1e2185212ce021
leehyokyun <gyrbs22@gmail.com>
2026-04-25 14:47:07 +0900

    Sub bloom filter handler with Service and Integration/Unit Test

--- src/main/java/com/redis/bottleneck/service/cache/RedisCachePenetrationSplitShardedSubFilterService.java
+++ src/main/java/com/redis/bottleneck/service/cache/RedisCachePenetrationSplitShardedSubFilterService.java
@@ -1,4 +1,75 @@
 package com.redis.bottleneck.service.cache;
 
-public class RedisCachePenetrationSplitShardedSubFilterService {
+import com.redis.bottleneck.common.bloomfilter.splitShardedSubBloomfilter.SplitShardedSubBloomfilter;
+import com.redis.bottleneck.common.bloomfilter.splitShardedSubBloomfilter.SplitShardedSubBloomfilterHandler;
+import com.redis.bottleneck.common.cache.service.RedisCacheService;
+import com.redis.bottleneck.common.cache.strategy.CacheStrategy;
+import com.redis.bottleneck.model.request.ArticleCreateRequest;
+import com.redis.bottleneck.model.request.ArticleUpdateRequest;
+import com.redis.bottleneck.model.response.ArticlePageResponse;
+import com.redis.bottleneck.model.response.ArticleResponse;
+import com.redis.bottleneck.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisCachePenetrationSplitShardedSubFilterService implements RedisCacheService {
+
+    private final ArticleService articleService;
+    private final SplitShardedSubBloomfilterHandler splitShardedSubBloomfilterHandler;
+
+    private static final SplitShardedSubBloomfilter splitShardedSubBloomfilter = SplitShardedSubBloomfilter.create(
+            "article-bloom-filter",
+            1000,
+            0.01,
+            4
+    );
+
+    @Override
+    public ArticleResponse read(Long articleId) {
+
+        Boolean result = splitShardedSubBloomfilterHandler.mightContain(splitShardedSubBloomfilter, String.valueOf(articleId));
+
+        return (result) ? articleService.read(articleId) : null;
+    }
+
+    @Override
+    public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
+        return articleService.readAll(boardId, page, pageSize);
+    }
+
+    @Override
+    public ArticlePageResponse readAllInfiniteScroll(Long boardId, Long lastArticleId, Long pageSize) {
+        return articleService.readInfiniteScroll(boardId, lastArticleId, pageSize);
+    }
+
+    @Override
+    public ArticleResponse create(ArticleCreateRequest articleCreateRequest) {
+
+        splitShardedSubBloomfilterHandler.add(splitShardedSubBloomfilter, String.valueOf(articleCreateRequest.articleId()));
+
+        return articleService.create(articleCreateRequest);
+    }
+
+    @Override
+    public ArticleResponse update(Long articleId, ArticleUpdateRequest articleUpdateRequest) {
+        return articleService.update(articleId, articleUpdateRequest);
+    }
+
+    @Override
+    public void delete(long articleId) {
+        articleService.delete(articleId);
+    }
+
+    @Override
+    public long count(long boardId) {
+        return articleService.count(boardId);
+    }
+
+    @Override
+    public boolean supports(CacheStrategy cacheStrategy) {
+        return CacheStrategy.SPLIT_SHARDED_SUB_BLOOM_FILTER == cacheStrategy;
+    }
 }
--- src/test/java/com/redis/bottleneck/service/strategy/subBloomfilter/RedisCacheShardedSplitSubBloomFilterTest.java
+++ src/test/java/com/redis/bottleneck/service/strategy/subBloomfilter/RedisCacheShardedSplitSubBloomFilterTest.java
@@ -1,4 +1,71 @@
 package com.redis.bottleneck.service.strategy.subBloomfilter;
 
-public class RedisCacheShardedSplitSubBloomFilterTest {
+import com.redis.bottleneck.common.bloomfilter.splitShardedBloomfilter.SplitShardedBloomfilter;
+import com.redis.bottleneck.common.bloomfilter.splitShardedSubBloomfilter.SplitShardedSubBloomfilter;
+import com.redis.bottleneck.common.bloomfilter.splitShardedSubBloomfilter.SplitShardedSubBloomfilterHandler;
+import com.redis.bottleneck.model.request.ArticleCreateRequest;
+import com.redis.bottleneck.service.ArticleService;
+import com.redis.bottleneck.service.cache.RedisCachePenetrationSplitShardedSubFilterService;
+import com.redis.bottleneck.utils.MySQLAndRedisIntegrationTestContainerSupportUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("test")
+public class RedisCacheShardedSplitSubBloomFilterTest extends MySQLAndRedisIntegrationTestContainerSupportUtil {
+
+    @Autowired
+    private RedisCachePenetrationSplitShardedSubFilterService redisCachePenetrationSplitShardedSubFilterService;
+
+    @MockitoSpyBean
+    private ArticleService articleService;
+
+    @MockitoSpyBean
+    private SplitShardedSubBloomfilterHandler splitShardedSubBloomfilterHandler;
+
+    @Test
+    void subBloomFilteringTest(){
+        //given
+        long boardId = 1L;
+
+        //when
+        for(long i = 0 ; i < 1000 ; i ++){
+            redisCachePenetrationSplitShardedSubFilterService.create(
+                    new ArticleCreateRequest( i + 1, "test data " + (i + 1), boardId)
+            );
+        }
+
+        //then
+        for(long articleId = 0 ; articleId < 1000 ; articleId++){
+            redisCachePenetrationSplitShardedSubFilterService.read(articleId + 1);
+        }
+
+        verify(articleService, times(1000)).read(anyLong());
+
+    }
+
+
+    //over testing
+
+//    private long getDataCountOfFilter(SplitShardedBloomfilter splitShardedBloomfilter) {
+//        String result = stringRedisTemplate.opsForValue().get("split-sharded-original-bloom-filter:data-count:%s".formatted(splitShardedBloomfilter.getId()));
+//
+//        return (result == null) ? 0 : Long.parseLong(result);
+//    }
+//
+//    private int getSubFilterCount(SplitShardedSubBloomfilter splitShardedSubBloomfilter) {
+//        String result = stringRedisTemplate.opsForValue().get("split-sharded-sub-bloom-filter-count:%s".formatted(splitShardedSubBloomfilter.getId()));
+//
+//        return (result == null) ? 0 : Integer.parseInt(result);
+//    }
 }
