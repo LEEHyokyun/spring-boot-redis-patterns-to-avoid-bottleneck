a67dcc6e26822c8cef723f65ec78f085b7212d00
leehyokyun <gyrbs22@gmail.com>
2026-04-27 18:15:32 +0900

    Jitter Handler / slice test

---
+++ src/main/java/com/redis/bottleneck/common/jitter/JitterHandler.java
@@ -0,0 +1,81 @@
+package com.redis.bottleneck.common.jitter;
+
+import com.redis.bottleneck.common.cache.handler.CacheHandler;
+import com.redis.bottleneck.common.cache.strategy.CacheStrategy;
+import com.redis.bottleneck.common.serde.DataSerializer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+import java.util.random.RandomGenerator;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JitterHandler implements CacheHandler {
+
+    private final StringRedisTemplate stringRedisTemplate;
+    private static final int JITTER_RANGE_SECONDS = 3; //변동성 3sec 이내
+
+    @Override
+    public <T> T fetch(String key, Duration ttl, Supplier<T> supplier, Class<T> clazz) {
+        String cached = stringRedisTemplate.opsForValue().get(key);
+
+        if(cached == null){
+            return this.refreshKey(key, ttl, supplier);
+        }
+
+        T data = DataSerializer.deserializeOrNull(cached, clazz);
+
+        if(data == null){
+            return this.refreshKey(key, ttl, supplier);
+        }
+
+        return data;
+    }
+
+    private <T> T refreshKey(String key, Duration ttl, Supplier<T> supplier) {
+        //supplier를 통해 datasource로 부터 데이터를 추출
+        T refreshed = supplier.get();
+        this.put(key, ttl, refreshed);
+
+        return refreshed;
+    }
+
+    @Override
+    public void put(String key, Duration ttl, Object value) {
+        stringRedisTemplate.opsForValue().set(key, DataSerializer.serializeOrException(value), this.jitteredTTl(ttl));
+    }
+
+    private Duration jitteredTTl(Duration ttl) {
+        /*
+        * JITTER = 3 sec이므로, 이를 적용하기 위해서는 최소한 3초 이상의 TTL이 필요
+        * */
+        if (ttl.getSeconds() <= JITTER_RANGE_SECONDS){
+            throw new IllegalArgumentException("Jittered TTL Should be more than "+ JITTER_RANGE_SECONDS);
+        }
+
+        /*
+        * -3 ~ +3 sec의 jitter를 ttl에 적용한 jittered TTl 추출.
+        * */
+        int jitter = RandomGenerator.getDefault().nextInt(-JITTER_RANGE_SECONDS, +JITTER_RANGE_SECONDS + 1);
+
+        log.info("jitter value : " + jitter);
+        log.info("finally jittered ttl value : " + ttl.plusSeconds(jitter));
+
+        return ttl.plusSeconds(jitter);
+    }
+
+    @Override
+    public void evict(String key) {
+        stringRedisTemplate.delete(key);
+    }
+
+    @Override
+    public boolean supports(CacheStrategy cacheStrategy) {
+        return CacheStrategy.Jitter == cacheStrategy;
+    }
+}
---
+++ src/test/java/com/redis/bottleneck/common/jitter/JitterHandlerTest.java
@@ -0,0 +1,117 @@
+package com.redis.bottleneck.common.jitter;
+
+import com.redis.bottleneck.utils.RedisTestContainerSupportUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@DataRedisTest
+@Import(JitterHandler.class)
+@Slf4j
+class JitterHandlerTest extends RedisTestContainerSupportUtil {
+
+    @Autowired
+    private JitterHandler jitterHandler;
+
+    @Test
+    void putTest(){
+        //given / when
+        jitterHandler.put(
+                "testKey1",
+                Duration.ofSeconds(10),
+                String.class
+        );
+        jitterHandler.put(
+                "testKey2",
+                Duration.ofSeconds(10),
+                String.class
+        );
+
+        //then
+        assertThat(stringRedisTemplate.getExpire("testKey1", TimeUnit.SECONDS)).isGreaterThan(7);
+        assertThat( stringRedisTemplate.getExpire("testKey2", TimeUnit.SECONDS)).isLessThan(13);
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionTest(){
+        //given / when / then
+        assertThrows(IllegalArgumentException.class, () -> {
+            jitterHandler.put(
+                    "testKey",
+                    Duration.ofSeconds(2),
+                    String.class
+            );
+        });
+    }
+
+    @Test
+    void cachingEvictTest(){
+        //given / when
+        jitterHandler.put(
+                "testKey"
+                ,Duration.ofSeconds(4),
+                String.class
+        );
+
+        //then
+        Assertions.assertNotNull(stringRedisTemplate.opsForValue().get("testKey"));
+
+        //given / when
+        jitterHandler.evict("testKey");
+
+        //then
+        Assertions.assertNull(stringRedisTemplate.opsForValue().get("testKey"));
+    }
+
+    @Test
+    void FetchTest() throws JsonProcessingException {
+        //given
+        String testKey = "testKey";
+
+        //stub
+        Supplier<String> supplier = Mockito.mock(Supplier.class);
+        when(supplier.get()).thenReturn(String.valueOf("original data"));
+
+        //when
+        jitterHandler.fetch(
+                "testKey",
+                Duration.ofSeconds(10),
+                () -> supplier.get(),
+                String.class
+        );
+        jitterHandler.fetch(
+                "testKey",
+                Duration.ofSeconds(10),
+                () -> supplier.get(),
+                String.class
+        );
+        jitterHandler.fetch(
+                "testKey",
+                Duration.ofSeconds(10),
+                () -> supplier.get(),
+                String.class
+        );
+
+        //then
+        Assertions.assertEquals("\"original data\"", stringRedisTemplate.opsForValue().get("testKey"));
+
+        //when
+        verify(supplier, times(1)).get();
+
+    }
+}
