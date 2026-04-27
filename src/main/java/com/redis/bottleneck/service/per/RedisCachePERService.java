ed6142fd8b609526dcf25e523f17651ae1d74984
leehyokyun <gyrbs22@gmail.com>
2026-04-28 00:16:17 +0900

    PERHandler with Service and Integration(Api Call) Test

---
+++ src/main/java/com/redis/bottleneck/service/per/RedisCachePERService.java
@@ -0,0 +1,81 @@
+package com.redis.bottleneck.service.per;
+
+import com.redis.bottleneck.common.cache.aop.CacheEvict;
+import com.redis.bottleneck.common.cache.aop.CachePut;
+import com.redis.bottleneck.common.cache.aop.Cacheable;
+import com.redis.bottleneck.common.cache.service.RedisCacheService;
+import com.redis.bottleneck.common.cache.strategy.CacheStrategy;
+import com.redis.bottleneck.common.per.PERHandler;
+import com.redis.bottleneck.model.request.ArticleCreateRequest;
+import com.redis.bottleneck.model.request.ArticleUpdateRequest;
+import com.redis.bottleneck.model.response.ArticlePageResponse;
+import com.redis.bottleneck.model.response.ArticleResponse;
+import com.redis.bottleneck.service.ArticleService;
+import io.lettuce.core.support.caching.RedisCache;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisCachePERService implements RedisCacheService {
+
+    private final ArticleService articleService;
+
+
+    @Override
+    @Cacheable(
+            cacheStrategy = CacheStrategy.PER,
+            cacheName = "article", //PER:article:~~
+            key = "#articleId",
+            ttl = 5
+    )
+    public ArticleResponse read(Long articleId) {
+        return articleService.read(articleId);
+    }
+
+    @Override
+    public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
+        return articleService.readAll(boardId, page, pageSize);
+    }
+
+    @Override
+    public ArticlePageResponse readAllInfiniteScroll(Long boardId, Long lastArticleId, Long pageSize) {
+        return articleService.readAll(boardId, lastArticleId, pageSize);
+    }
+
+    @Override
+    public ArticleResponse create(ArticleCreateRequest articleCreateRequest) {
+        return articleService.create(articleCreateRequest);
+    }
+
+    @Override
+    @CachePut(
+            cacheStrategy = CacheStrategy.PER,
+            cacheName = "article",
+            key = "#articleId",
+            ttl = 5
+    )
+    public ArticleResponse update(Long articleId, ArticleUpdateRequest articleUpdateRequest) {
+        return articleService.update(articleId, articleUpdateRequest);
+    }
+
+    @Override
+    @CacheEvict(
+            cacheStrategy = CacheStrategy.PER,
+            cacheName = "article",
+            key = "#articleId"
+    )
+    public void delete(long articleId) {
+        articleService.delete(articleId);
+    }
+
+    @Override
+    public long count(long boardId) {
+        return articleService.count(boardId);
+    }
+
+    @Override
+    public boolean supports(CacheStrategy cacheStrategy) {
+        return CacheStrategy.PER == cacheStrategy;
+    }
+}
---
+++ src/test/java/com/redis/bottleneck/service/strategy/per/RedisCachePERTest.java
@@ -0,0 +1,88 @@
+package com.redis.bottleneck.service.strategy.per;
+
+import com.redis.bottleneck.common.cache.strategy.CacheStrategy;
+import com.redis.bottleneck.model.request.ArticleCreateRequest;
+import com.redis.bottleneck.model.response.ArticleResponse;
+import com.redis.bottleneck.service.ArticleService;
+import com.redis.bottleneck.service.per.RedisCachePERService;
+import com.redis.bottleneck.utils.ArticleEndPointCallUtil;
+import com.redis.bottleneck.utils.EndPointCallUtil;
+import com.redis.bottleneck.utils.MySQLAndRedisIntegrationTestContainerSupportUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("test")
+public class RedisCachePERTest extends MySQLAndRedisIntegrationTestContainerSupportUtil {
+
+    @Autowired
+    private RedisCachePERService redisCachePERService;
+
+    @MockitoSpyBean
+    private ArticleService articleService;
+
+    @Test
+    void PERTest(){
+        //gvien
+        String cacheStrategy = CacheStrategy.PER.name();
+        String cacheName = "article";
+        List<String> keys = IntStream.range(0, 100).mapToObj(i -> "PER:article:" + i).toList();
+        long boardId = 1L;
+
+        //when
+        for(long i = 0 ; i < 50 ; i++){
+            redisCachePERService.create(
+                    new ArticleCreateRequest(
+                            i,
+                            "test data" + i,
+                            boardId
+                    )
+            );
+        }
+
+        //then
+        for(long i = 0 ; i < 10 ; i++){
+            for(long j = 0 ; j < 50 ; j++){
+                redisCachePERService.read(j);
+            }
+        }
+
+        verify(articleService, times(50)).read(Mockito.anyLong());
+
+    }
+
+    @Test
+    void ApiCallTest(){
+        //given
+        CacheStrategy cacheStrategy = CacheStrategy.PER;
+
+        //when
+        for(long i = 0 ; i < 100 ; i++){
+            ArticleResponse articleResponse = ArticleEndPointCallUtil.create(cacheStrategy, new ArticleCreateRequest(
+                    i,
+                    "test data " + i,
+                    1L
+            ));
+        };
+
+        for(long i = 0 ; i < 10 ; i++){
+            for(long j = 0 ; j < 100 ; j++){
+                ArticleEndPointCallUtil.read(cacheStrategy, j);
+            }
+        }
+
+        //then : log 확인
+    }
+}
